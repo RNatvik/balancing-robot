f42bbca9d0b9b6a24ff696653fee9ac09f682a3c
RNatvik <ronatvik@gmail.com>
2020-08-22 19:15:09 +0200

    added "helpers" package containing scripts such as killer.py and single_pub.py

---
+++ src/helpers/single_pub.py
@@ -0,0 +1,19 @@
+import proccom
+import time
+
+
+def format_msg():
+    return {'transmit_rate': 2000}
+
+
+def main():
+    publisher = proccom.Publisher('arduino_cmd', 'test_pub', format_msg)
+    publisher.connect()
+    time.sleep(1)
+    publisher.publish()
+    time.sleep(1)
+    publisher.stop()
+
+
+if __name__ == '__main__':
+    main()
